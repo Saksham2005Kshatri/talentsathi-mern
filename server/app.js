bdd56c6568d3d9ebd25e744733036b44fa4de35b
ksaksham23 <ksaksham23@tbc.edu.np>
2024-05-19 18:42:09 +0545

    update package.json for nodemon

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -19,9 +20,18 @@ app.use(express.urlencoded({ extended: true })); // for sending form data
 
 app.use("/api/auth", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+if (process.env.NODE_ENV == "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Hello world");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
