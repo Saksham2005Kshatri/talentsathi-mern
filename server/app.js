907d0097cfb8268111fcd3e6516cc2826cc29d7a
ksaksham23 <ksaksham23@tbc.edu.np>
2024-05-20 12:30:35 +0545

    add path

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
@@ -22,9 +23,16 @@ app.use(express.urlencoded({ extended: true })); // for sending form data
 
 app.use("/api/auth", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("<h1>Hello world</h1>"));
+}
 
 app.use(notFound);
 app.use(errorHandler);
